28c091b6e9163d8f1e8fd8288718efd8fcc9d819
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2024-06-16 14:58:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akbotmakersnew/AutoFilterAdvance
+  git clone https://github.com/Akbotmakersnew/AutoFilterAdvance /AutoFilterAdvance
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterAdvance
